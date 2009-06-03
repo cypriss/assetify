dc831bb80b818eca6318f924c49c98bf9b7936b8
Jonathan Novak <jnovak@netbooks.com>
2009-06-03 15:56:40 -0700

    New rails format for init.rb'ing

--- init.rb
+++ init.rb
@@ -1,12 +1,1 @@
-# Include hook code here
-
-
-require "assetify"
-require "assetify_secretary"
-require "assetify_compressor"
-require "assetify_helper"
- 
-class ActionController::Base
-  helper :assetify
-end
-
+require File.dirname(__FILE__) + "/rails/init.rb"
